e22c4ced6d67941ac187a7d1e1226c04c0eb47c9
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2025-06-07 13:17:03 +0200

    fix/backend/

--- backend/main.py
+++ backend/main.py
@@ -25,12 +25,12 @@ print("Frontend URL", frontend_url)
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    frontend_url  
+    frontend_url,
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],     
     allow_headers=["*"],     
