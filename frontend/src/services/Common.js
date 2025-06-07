c8b87236825d0f8d930a5661cc6a99a1de2c9e24
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2025-06-07 12:42:57 +0200

    fix/frontend/Common Service

--- frontend/src/services/Common.js
+++ frontend/src/services/Common.js
@@ -1,9 +1,14 @@
 import { auth } from "./Storage";
 
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+let isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+// isLocal = false;
 
-const API_BASE_URL = import.meta.env.BACKEND_URL;
-export const API_BASE_DOMAIN = isLocal ? "http://localhost:8000" : API_BASE_URL;
+const envBackendUrl = import.meta.env.VITE_BACKEND_URL;
+export const API_BASE_DOMAIN = isLocal ? "http://localhost:8000" : envBackendUrl;
+
+// console.log("envBackendUrl:", envBackendUrl);
+
+// console.log("API_BASE_DOMAIN", API_BASE_DOMAIN);
 
 export function isFriendlyWithField(field) {
   const isFriendlyWithField = ["dogs", "cats", "kids", "adults"].includes(field);
